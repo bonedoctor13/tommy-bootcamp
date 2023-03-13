29671828852f2c53d6ed50477505f96feb792c05
JT <51496335+bonedoctor13@users.noreply.github.com>
2023-03-13 23:05:59 +0000

    render tictactoe board as table with buttons to make move - buttons dont work yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './tictactoe'
+import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils';
+import { initialState, jsxElementForGameState, state3 } from './tictactoe';
 
 function App() {
+  // const [gameState, setGameState] = useState(initialState)
+  const gameJSXElement = jsxElementForGameState(initialState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Johnny</p>
-        {/* <p><marquee>Johnny</marquee></p> */}
-      </header>
       <header className="App-body">
-        <dl><dt>CSS (Cascading Style Sheets)</dt><dd>a style sheet language used for describing the look and formatting of a document written in markup language</dd></dl>
-        <dl><dt>image</dt><dd>A serialized copy of the entire state of a computer system stored in some non-volatile form such as a file. A system is said to be capable of using system images if it can be shut down and later restored to exactly the same state.</dd></dl>
-        <dl><dt>container</dt><dd>An operating system-level virtualization or application-level virtualization over multiple network resources so that software applications can run in isolated user spaces called containers in any cloud or non-cloud environment, regardless of type or vendor.</dd></dl>
-        <dl><dt>volume</dt><dd>A single accessible storage area with a single file system, typically (though not necessarily) resident on a single partition of a hard disk. Although a volume might be different from a physical disk drive, it can still be accessed with an operating system's logical interface. However, a volume differs from a partition.</dd></dl>
+        <th>Welcome to Tic Tac Toe!</th>
+        <br></br>
+        { gameJSXElement }
       </header>
     </div>
   );
 }
 
 export default App;
+
+// td in tr, function that imports initial game board, then exports move back to program to re-export new gameboard to WritableStreamDefaultWriter, div to make Player X, Player O, and error message.
+
+{{/* <table>
+<tr>_ | _ | _ </tr>
+<tr>_ | _ | _ </tr>
+<tr>_ | _ | _ </tr>
+</table> */}}
+
+/* under 1st div
+<header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>Johnny</p>
+</header> */
---
+++ src/tictactoe.tsx
@@ -0,0 +1,129 @@
+type JSXElement = JSX.Element
+type Player = "X" | "O"
+type Square = Player | "_"
+type Line = Array<Square>
+type Row = Line
+type Column = Line
+type Board = Array<Row>
+type GetSquare = (b:Board, x:number, y:number) => Square
+type Move = {
+    x:number
+    y:number
+}
+type MakeMove = (gs:GameState, m:Move) => GameState
+type SwitchPlayer = (p:Player) => Player
+type GameState = {
+    board: Board,
+    player: Player,
+    errorMessage?:string,
+}
+
+const switchPlayer:SwitchPlayer = (p) => p == "X" ? "O" : "X"
+const x = "X"
+const o = "O"
+const _ = "_"
+const initialBoard:Board = [
+    [_,_,_],
+    [_,_,_],
+    [_,_,_],
+]
+
+const getSquare: GetSquare = (b,x,y) => b[y][x]
+export const initialState: GameState = {
+    board: initialBoard,
+    player: x
+}
+
+const makeMove: MakeMove = (gs, move) => {
+    if(getSquare(gs.board, move.x, move.y) != _) {
+        return {
+            board: gs.board,
+            player: gs.player,
+            errorMessage: "Invalid move"
+        }
+    } else {
+        const mapSquare = (square:Square, squareIndex:number):Square => squareIndex == move.x ? gs.player : square
+        const mapRow = (row:Row, rowIndex:number):Row => rowIndex == move.y ? row.map(mapSquare) : row
+        return {
+            board: gs.board.map(mapRow),
+            player: switchPlayer(gs.player),
+            errorMessage: undefined,
+        }
+    }
+}
+
+const throwNotImplemented = () => { throw new Error("Not Implemented")  }
+const rows = (b:Board):Array<Row> => b
+const columns = (b:Board):Array<Column> => throwNotImplemented()
+const diagonals = (b:Board):Array<Line> => throwNotImplemented()
+const winLines = (b:Board):Array<Line> => [...diagonals(b), ...columns(b), ...rows(b)]
+
+type Predicate<T> = (t:T) => boolean
+
+const isX:Predicate<Square> = square => square == x 
+const isO:Predicate<Square> = square => square == o
+
+type Reducer<T, S> = (accumulatedValue:S, currentValue:T) => S
+type Reduce = <T, S>(items:Array<T>, initialValue:S, reducer:Reducer<T, S>) => S
+
+const reduce:Reduce =  (items, initialValue, reducer) => items.reduce(reducer, initialValue)
+
+type ArrayPredicate = <T>(items:Array<T>, predicate:Predicate<T>) => boolean
+type Any = ArrayPredicate
+const any:Any = (items, predicate) => items.some(predicate)
+
+type All = ArrayPredicate
+const all:All = (items, predicate) => items.every(predicate)
+
+const hasPlayerWon = (playerPredicate:Predicate<Square>) => 
+    (b:Board) => any(winLines(b), line => all(line, playerPredicate))
+
+const hasXWon = hasPlayerWon(isX)
+const hasOWon = hasPlayerWon(isO)
+
+
+// View Logic Below
+
+const logGamestate = (gameState:GameState) => {
+    if(gameState.errorMessage) console.log(`ERROR! ${gameState.errorMessage}`)
+    console.log(gameState.board.join('\n'))
+    console.log(`Current Player: ${gameState.player}`)
+    console.log("")
+} 
+
+logGamestate(initialState)
+
+const move:Move = { x: 1, y: 1 }
+const state2 = makeMove(initialState, move)
+logGamestate(state2)
+
+const move2:Move = { x: 2, y: 0 }
+export const state3 = makeMove(state2, move)
+logGamestate(state3)
+
+type JSXElementForGameState = (gameState: GameState) => JSXElement
+
+const jsxElementForSquare = (square:Square):JSXElement => (
+    <>
+        { square == _ ? <button onClick={alert}>_</button> : square }
+    </>
+)
+
+const jsxElementForRow = (row:Row):JSXElement => (
+    <>
+        {row.map(square => <td>{jsxElementForSquare(square)}</td>)}
+    </>
+)
+
+export const jsxElementForGameState:JSXElementForGameState = gs => (
+    <>
+        <table>
+            {
+                rows(gs.board).map(row => <tr>{jsxElementForRow(row)}</tr>)
+            }
+        </table>
+        <br></br>
+        {gs.errorMessage ? <div>Error {gs.errorMessage}</div> : null}
+        <div> Current Player: {gs.player}</div>
+    </>
+)
